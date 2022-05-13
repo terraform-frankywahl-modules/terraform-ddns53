7574c77e394b8d43ab1f5f7f0098dd07e0759a71
Franky W <frankywahl@users.noreply.github.com>
2022-05-13 19:13:24 +0200

    Get IP from request

--- code/web/web.go
+++ code/web/web.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"strings"
 
@@ -44,13 +45,46 @@ func (s *Server) routes() {
 }
 
 func (s *Server) ddns53(w http.ResponseWriter, r *http.Request) {
-	ip, ok := r.URL.Query()["myip"]
-	if !ok {
+	getIP := func(r *http.Request) (string, error) {
+		//Get IP from the X-REAL-IP header
+		ip := r.Header.Get("X-REAL-IP")
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+
+		//Get IP from X-FORWARDED-FOR header
+		ips := r.Header.Get("X-FORWARDED-FOR")
+		splitIps := strings.Split(ips, ",")
+		for _, ip := range splitIps {
+			netIP := net.ParseIP(ip)
+			if netIP != nil {
+				return ip, nil
+			}
+		}
+
+		//Get IP from RemoteAddr
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			return "", err
+		}
+		netIP = net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+		return "", fmt.Errorf("could not fine a valid IP")
+
+	}
+
+	ip, err := getIP(r)
+	if err != nil {
+		s.logger.Infof("could not get IP")
 		w.WriteHeader(http.StatusUnprocessableEntity)
+		fmt.Fprintf(w, err.Error())
 		return
 	}
 
-	if err := ddns53.UpdateRecord(r.Context(), s.zoneID, s.fqdn, ip[0]); err != nil {
+	if err := ddns53.UpdateRecord(r.Context(), s.zoneID, s.fqdn, ip); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, err.Error())
 		return
